9da5474f91f95d40db45635d6289bf23961f5588
linux_china <libing.chen@gmail.com>
2021-02-03 15:08:29 -0800

    DummyServiceRegistry

---
+++ client-app/src/main/java/com/vinsguru/client/config/impl/DummyServiceRegistry.java
@@ -0,0 +1,37 @@
+package com.vinsguru.client.config.impl;
+
+import com.vinsguru.client.config.RSocketServerInstance;
+import com.vinsguru.client.config.RSocketServiceRegistry;
+import io.rsocket.loadbalance.LoadbalanceTarget;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Sinks;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@ConfigurationProperties(prefix = "rsocket.square-service")
+public class DummyServiceRegistry implements RSocketServiceRegistry {
+
+    private List<RSocketServerInstance> servers;
+    private Sinks.Many<List<RSocketServerInstance>> registry = Sinks.many().replay().latest();
+
+    public void setServers(List<RSocketServerInstance> servers) {
+        this.servers = servers;
+        this.registry.tryEmitNext(servers);
+    }
+
+    public Flux<List<LoadbalanceTarget>> getServers() {
+        return registry.asFlux()
+                .map(this::toLoadBalanceTarget);
+    }
+
+    private List<LoadbalanceTarget> toLoadBalanceTarget(List<RSocketServerInstance> rSocketServers) {
+        return rSocketServers.stream()
+                .map(server -> LoadbalanceTarget.from(server.getHost() + server.getPort(), server.constructClientTransport()))
+                .collect(Collectors.toList());
+    }
+
+}
