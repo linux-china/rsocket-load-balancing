b81a598ad2de2fceeb522d362a2ec91539407142
linux_china <libing.chen@gmail.com>
2021-02-03 15:09:39 -0800

    RSocketServiceRegistry

---
+++ client-app/src/main/java/com/vinsguru/client/config/RSocketServiceRegistry.java
@@ -0,0 +1,10 @@
+package com.vinsguru.client.config;
+
+import io.rsocket.loadbalance.LoadbalanceTarget;
+import reactor.core.publisher.Flux;
+
+import java.util.List;
+
+public interface RSocketServiceRegistry {
+    Flux<List<LoadbalanceTarget>> getServers();
+}
