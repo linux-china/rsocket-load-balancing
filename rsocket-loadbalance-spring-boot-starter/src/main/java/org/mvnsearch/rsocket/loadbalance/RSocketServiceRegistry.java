c01649ac7d40317015aa44ef614fbb318da0ec6f
linux_china <libing.chen@gmail.com>
2021-02-03 21:02:37 -0800

    Add getSnapshots()

--- rsocket-loadbalance-spring-boot-starter/src/main/java/org/mvnsearch/rsocket/loadbalance/RSocketServiceRegistry.java
+++ rsocket-loadbalance-spring-boot-starter/src/main/java/org/mvnsearch/rsocket/loadbalance/RSocketServiceRegistry.java
@@ -5,9 +5,12 @@ import org.springframework.messaging.rsocket.RSocketRequester;
 import reactor.core.publisher.Flux;
 
 import java.util.List;
+import java.util.Map;
 
 public interface RSocketServiceRegistry {
     Flux<List<LoadbalanceTarget>> getServers(String serviceName);
 
     RSocketRequester buildLoadBalanceRSocket(String serviceName, RSocketRequester.Builder builder);
+
+    Map<String, List<RSocketServerInstance>> getSnapshots();
 }
