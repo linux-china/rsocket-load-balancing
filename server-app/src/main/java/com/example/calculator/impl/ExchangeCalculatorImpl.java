1875607a25ec0e5939884bf07091c4862222a5d3
linux_china <libing.chen@gmail.com>
2021-02-04 14:05:36 -0800

    Polishing

--- server-app/src/main/java/com/example/calculator/impl/ExchangeCalculatorImpl.java
+++ server-app/src/main/java/com/example/calculator/impl/ExchangeCalculatorImpl.java
@@ -4,15 +4,16 @@ import com.example.calculator.ExchangeCalculatorService;
 import com.example.calculator.ExchangeRequest;
 import com.example.calculator.annotations.RSocketHandler;
 import com.example.calculator.annotations.RSocketService;
+import reactor.core.publisher.Mono;
 
 @RSocketService("com.example.calculator.ExchangeCalculatorService")
 public class ExchangeCalculatorImpl implements ExchangeCalculatorService {
     @Override
     @RSocketHandler("exchange")
-    public double exchange(ExchangeRequest request) {
+    public Mono<Double> exchange(ExchangeRequest request) {
         if (request.getSource().equals("USD") && request.getTarget().equals("CNY")) {
-            return request.getAmount() * 6.4;
+            return Mono.just(request.getAmount() * 6.4);
         }
-        return 0.0;
+        return Mono.just(0.0);
     }
 }
