80c136e1638f8020dfbbebe330457f7686809e5f
linux_china <libing.chen@gmail.com>
2021-02-03 19:20:29 -0800

    RSocketRemoteServiceBuilder

---
+++ client-app/src/main/java/com/example/rsocket/client/proxy/RSocketRemoteServiceBuilder.java
@@ -0,0 +1,36 @@
+package com.example.rsocket.client.proxy;
+
+import org.springframework.messaging.rsocket.RSocketRequester;
+
+import java.lang.reflect.Proxy;
+
+public class RSocketRemoteServiceBuilder<T> {
+    private String serviceName;
+    private Class<?> serviceInterface;
+    private RSocketRequester rsocketRequester;
+
+    public RSocketRemoteServiceBuilder<T> serviceInterface(Class<?> serviceInterface) {
+        this.serviceInterface = serviceInterface;
+        return this;
+    }
+
+    public RSocketRemoteServiceBuilder<T> serviceName(String serviceName) {
+        this.serviceName = serviceName;
+        return this;
+    }
+
+    public RSocketRemoteServiceBuilder<T> rsocketRequester(RSocketRequester rsocketRequester) {
+        this.rsocketRequester = rsocketRequester;
+        return this;
+    }
+
+    @SuppressWarnings("unchecked")
+    public T build() {
+        RSocketRemoteCallInvocationHandler handler = new RSocketRemoteCallInvocationHandler(rsocketRequester, serviceName);
+        return (T) Proxy.newProxyInstance(
+                serviceInterface.getClassLoader(),
+                new Class[]{serviceInterface},
+                handler);
+    }
+
+}
