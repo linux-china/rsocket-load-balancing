f409cd61090ac2f931ae53483ea086f9e8cca4c4
linux_china <libing.chen@gmail.com>
2021-02-03 20:36:18 -0800

    Add buildLoadBalanceRSocket()

--- rsocket-loadbalance-spring-boot-starter/src/main/java/org/mvnsearch/rsocket/loadbalance/RSocketServiceRegistry.java
+++ rsocket-loadbalance-spring-boot-starter/src/main/java/org/mvnsearch/rsocket/loadbalance/RSocketServiceRegistry.java
@@ -1,10 +1,13 @@
-package com.example.rsocket.client.config;
+package org.mvnsearch.rsocket.loadbalance;
 
 import io.rsocket.loadbalance.LoadbalanceTarget;
+import org.springframework.messaging.rsocket.RSocketRequester;
 import reactor.core.publisher.Flux;
 
 import java.util.List;
 
 public interface RSocketServiceRegistry {
     Flux<List<LoadbalanceTarget>> getServers(String serviceName);
+
+    RSocketRequester buildLoadBalanceRSocket(String serviceName, RSocketRequester.Builder builder);
 }
