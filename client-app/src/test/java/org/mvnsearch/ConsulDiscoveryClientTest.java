6c06e5e6e176ae36fab9ac7ebc6e21cd5b2a1294
linux_china <libing.chen@gmail.com>
2021-02-03 18:02:13 -0800

    Introduce ConsulReactiveDiscoveryClient

--- client-app/src/test/java/org/mvnsearch/ConsulDiscoveryClientTest.java
+++ client-app/src/test/java/org/mvnsearch/ConsulDiscoveryClientTest.java
@@ -5,8 +5,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.commons.util.InetUtils;
 import org.springframework.cloud.commons.util.InetUtilsProperties;
-import org.springframework.cloud.consul.discovery.ConsulDiscoveryClient;
 import org.springframework.cloud.consul.discovery.ConsulDiscoveryProperties;
+import org.springframework.cloud.consul.discovery.reactive.ConsulReactiveDiscoveryClient;
 
 import java.util.List;
 
@@ -15,8 +15,8 @@ public class ConsulDiscoveryClientTest {
     public void testFindServiceServers() {
         ConsulClient consulClient = new ConsulClient("127.0.0.1", 8500);
         ConsulDiscoveryProperties properties = new ConsulDiscoveryProperties(new InetUtils(new InetUtilsProperties()));
-        ConsulDiscoveryClient discoveryClient = new ConsulDiscoveryClient(consulClient, properties);
-        List<ServiceInstance> instances = discoveryClient.getInstances("com-example-CalculatorService");
+        ConsulReactiveDiscoveryClient discoveryClient = new ConsulReactiveDiscoveryClient(consulClient, properties);
+        List<ServiceInstance> instances = discoveryClient.getInstances("com-example-CalculatorService").collectList().block();
         for (ServiceInstance instance : instances) {
             System.out.println("Host: " + instance.getHost());
             System.out.println("RSocket Port:" + instance.getMetadata().get("rsocketPort"));
