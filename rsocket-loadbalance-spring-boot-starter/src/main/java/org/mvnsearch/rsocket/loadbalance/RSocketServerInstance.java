b1daa2f379a781cfd07b69ab557b907ebccee925
linux_china <libing.chen@gmail.com>
2021-02-03 20:42:46 -0800

    Add equals & hashCode

--- rsocket-loadbalance-spring-boot-starter/src/main/java/org/mvnsearch/rsocket/loadbalance/RSocketServerInstance.java
+++ rsocket-loadbalance-spring-boot-starter/src/main/java/org/mvnsearch/rsocket/loadbalance/RSocketServerInstance.java
@@ -34,4 +34,20 @@ public class RSocketServerInstance {
     public ClientTransport constructClientTransport() {
         return TcpClientTransport.create(host, port);
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        RSocketServerInstance that = (RSocketServerInstance) o;
+        if (port != that.port) return false;
+        return host.equals(that.host);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = host.hashCode();
+        result = 31 * result + port;
+        return result;
+    }
 }
