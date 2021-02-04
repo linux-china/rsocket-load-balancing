b794ad6b61fe1e3f9ced6abe11535a75a89b3819
linux_china <libing.chen@gmail.com>
2021-02-03 21:01:58 -0800

    RSocketLoadBalanceEndpoint

---
+++ rsocket-loadbalance-spring-boot-starter/src/main/java/org/mvnsearch/rsocket/loadbalance/RSocketLoadBalanceEndpoint.java
@@ -0,0 +1,24 @@
+package org.mvnsearch.rsocket.loadbalance;
+
+import org.springframework.boot.actuate.endpoint.annotation.Endpoint;
+import org.springframework.boot.actuate.endpoint.annotation.ReadOperation;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Endpoint(id = "rsocketlb")
+public class RSocketLoadBalanceEndpoint {
+
+    private RSocketServiceRegistry rsocketServiceRegistry;
+
+    public RSocketLoadBalanceEndpoint(RSocketServiceRegistry rsocketServiceRegistry) {
+        this.rsocketServiceRegistry = rsocketServiceRegistry;
+    }
+
+    @ReadOperation
+    public Map<String, Object> info() {
+        Map<String, Object> info = new HashMap<>();
+        info.put("services", rsocketServiceRegistry.getSnapshots());
+        return info;
+    }
+}
