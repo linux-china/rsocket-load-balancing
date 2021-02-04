21afad6e8e85b1258acb0b641a341853c4f0f70d
linux_china <libing.chen@gmail.com>
2021-02-03 18:02:25 -0800

    Add @EnableDiscoveryClient

--- client-app/src/main/java/com/vinsguru/client/RSocketClientApplication.java
+++ client-app/src/main/java/com/vinsguru/client/RSocketClientApplication.java
@@ -1,30 +1,15 @@
 package com.vinsguru.client;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.messaging.rsocket.RSocketRequester;
-import reactor.core.publisher.Flux;
-
-import java.time.Duration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-public class RSocketClientApplication implements CommandLineRunner {
-
-    @Autowired
-    private RSocketRequester rSocketRequester;
+@EnableDiscoveryClient
+public class RSocketClientApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(RSocketClientApplication.class, args);
     }
 
-    @Override
-    public void run(String... args) {
-        Flux.range(1, 6)
-                .delayElements(Duration.ofMillis(100))
-                .flatMap(i -> rSocketRequester.route("com.example.CalculatorService.square").data(i).retrieveMono(Integer.class).retry(1))
-                .doOnNext(i -> System.out.println("Response : " + i))
-                .blockLast();
-    }
 }
