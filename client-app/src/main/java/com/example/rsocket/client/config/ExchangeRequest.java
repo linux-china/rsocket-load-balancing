bd3616cc94a6907cfc3a2b35242553bccd886703
linux_china <libing.chen@gmail.com>
2021-02-04 14:01:04 -0800

    ExchangeRequest

---
+++ client-app/src/main/java/com/example/rsocket/client/config/ExchangeRequest.java
@@ -0,0 +1,40 @@
+package com.example.rsocket.client.config;
+
+public class ExchangeRequest {
+    private double amount;
+    private String source;
+    private String target;
+
+    public ExchangeRequest() {
+    }
+
+    public ExchangeRequest(double amount, String source, String target) {
+        this.amount = amount;
+        this.source = source;
+        this.target = target;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public String getSource() {
+        return source;
+    }
+
+    public void setSource(String source) {
+        this.source = source;
+    }
+
+    public String getTarget() {
+        return target;
+    }
+
+    public void setTarget(String target) {
+        this.target = target;
+    }
+}
