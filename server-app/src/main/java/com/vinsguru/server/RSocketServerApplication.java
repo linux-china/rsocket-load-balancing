e468d45d327686d95655abf44ff3559effc60c07
linux_china <libing.chen@gmail.com>
2021-02-03 17:44:10 -0800

    Add @EnableDiscoveryClient

--- server-app/src/main/java/com/vinsguru/server/RSocketServerApplication.java
+++ server-app/src/main/java/com/vinsguru/server/RSocketServerApplication.java
@@ -2,8 +2,10 @@ package com.vinsguru.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class RSocketServerApplication {
 
     public static void main(String[] args) {
