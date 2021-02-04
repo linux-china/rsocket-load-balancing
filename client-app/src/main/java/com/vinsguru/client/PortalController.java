9e01a2a255c0105b4490e14ee5d4f64d81c7765e
linux_china <libing.chen@gmail.com>
2021-02-03 17:58:56 -0800

    Polishing

--- client-app/src/main/java/com/vinsguru/client/PortalController.java
+++ client-app/src/main/java/com/vinsguru/client/PortalController.java
@@ -3,19 +3,17 @@ package com.vinsguru.client;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.rsocket.RSocketRequester;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
-
-import java.util.Random;
 
 @RestController
 public class PortalController {
     @Autowired
     private RSocketRequester calculatorRequester;
 
-    @GetMapping("/")
-    public Mono<String> index() {
-        int number = new Random().nextInt(20);
+    @GetMapping("/square/{number}")
+    public Mono<String> index(@PathVariable("number") int number) {
         return calculatorRequester.route("com.example.CalculatorService.square")
                 .data(number)
                 .retrieveMono(Integer.class)
