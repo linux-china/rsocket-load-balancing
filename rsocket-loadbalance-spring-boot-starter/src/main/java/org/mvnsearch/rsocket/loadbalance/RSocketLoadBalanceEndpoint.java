7bcac54624624cc616b6aeede4a72e71416e4789
linux_china <libing.chen@gmail.com>
2021-02-03 21:35:10 -0800

    Polishing

--- rsocket-loadbalance-spring-boot-starter/src/main/java/org/mvnsearch/rsocket/loadbalance/RSocketLoadBalanceEndpoint.java
+++ rsocket-loadbalance-spring-boot-starter/src/main/java/org/mvnsearch/rsocket/loadbalance/RSocketLoadBalanceEndpoint.java
@@ -4,12 +4,13 @@ import org.springframework.boot.actuate.endpoint.annotation.Endpoint;
 import org.springframework.boot.actuate.endpoint.annotation.ReadOperation;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 @Endpoint(id = "rsocketlb")
 public class RSocketLoadBalanceEndpoint {
-
-    private RSocketServiceRegistry rsocketServiceRegistry;
+    private final RSocketServiceRegistry rsocketServiceRegistry;
 
     public RSocketLoadBalanceEndpoint(RSocketServiceRegistry rsocketServiceRegistry) {
         this.rsocketServiceRegistry = rsocketServiceRegistry;
@@ -18,7 +19,11 @@ public class RSocketLoadBalanceEndpoint {
     @ReadOperation
     public Map<String, Object> info() {
         Map<String, Object> info = new HashMap<>();
-        info.put("services", rsocketServiceRegistry.getSnapshots());
+        Map<String, List<String>> services = new HashMap<>();
+        for (Map.Entry<String, List<RSocketServerInstance>> entry : rsocketServiceRegistry.getSnapshots().entrySet()) {
+            services.put(entry.getKey(), entry.getValue().stream().map(RSocketServerInstance::getURI).collect(Collectors.toList()));
+        }
+        info.put("services", services);
         info.put("lastRefreshAt", rsocketServiceRegistry.getLastRefreshTimestamp());
         return info;
     }
