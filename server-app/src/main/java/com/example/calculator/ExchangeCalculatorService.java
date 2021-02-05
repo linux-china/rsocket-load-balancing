f59bc5d6923dc8e8e3f0406a1022a8d8952a69e3
linux_china <libing.chen@gmail.com>
2021-02-05 11:58:46 -0800

    Moved to calculator-api module

--- server-app/src/main/java/com/example/calculator/ExchangeCalculatorService.java
+++
@@ -1,8 +0,0 @@
-package com.example.calculator;
-
-import reactor.core.publisher.Mono;
-
-public interface ExchangeCalculatorService {
-
-    Mono<Double> exchange(ExchangeRequest request);
-}
--- server-app/src/main/java/com/example/calculator/MathCalculatorService.java
+++
@@ -1,7 +0,0 @@
-package com.example.calculator;
-
-import reactor.core.publisher.Mono;
-
-public interface MathCalculatorService {
-    Mono<Integer> square(Integer input);
-}
